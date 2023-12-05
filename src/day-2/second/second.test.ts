19a67cfcaafbd1047923d98e3f71ab43f51847e7
Parro <29497+Parro@users.noreply.github.com>
2023-12-05 09:07:41 +0100

    feat: Day 2 second exercise

---
+++ src/day-2/second/second.test.ts
@@ -0,0 +1,20 @@
+import { describe, it } from 'node:test';
+import { strict as assert } from 'node:assert';
+
+import second from './second';
+
+describe('Day 2 second exercise tests', () => {
+  it('should get the  fewest number of cubes of each color that could have been in the bag to make the game possible and multiply the game number', () => {
+    const input = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+    Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+    Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
+
+    const expectedOutput = 2286;
+ 
+    const output = second(input);
+
+    assert.strictEqual(output, expectedOutput);
+  });
+});
---
+++ src/day-2/second/second.ts
@@ -0,0 +1,42 @@
+type Second = (input: string) => number;
+
+const second: Second = (input) => {
+  // Split input into rows
+  const rows = input.split('\n');
+
+  return rows.reduce((tot, row) => {
+    // Get game number
+    const gameNumber = row.match(/Game ([0-9]*)/)?.[1];
+
+    // Get single balls extraction
+    const ballsExtractions = row
+      .replace(`Game ${gameNumber}:`, '')
+      .split(';')
+      .flatMap((gameExtraction) => gameExtraction.split(','));
+
+      // Get the fewer number of balls per color to make the game possible
+    const fewerBallsPerColor = ballsExtractions.reduce<{
+      [key: string]: number;
+    }>((acc, ballsExtraction) => {
+      const [number, color] = ballsExtraction.trim().split(' ');
+
+      if (acc[color] === undefined || Number(number) > acc[color]) {
+        acc[color] = Number(number);
+      }
+
+      return acc;
+    }, {});
+
+    // Multiply the number of balls
+    const multiplyBalls = Object.values(fewerBallsPerColor).reduce(
+      (cube, balls) => cube * balls,
+      1
+    );
+
+    tot += multiplyBalls;
+
+    return tot;
+  }, 0);
+};
+
+export default second;
