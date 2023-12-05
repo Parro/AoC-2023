14135236e2331d3a50f983dad2e9a0ff9a33e667
Parro <29497+Parro@users.noreply.github.com>
2023-12-05 09:09:50 +0100

    refactor: Improve test descriptions

--- src/day-1/first/first.test.ts
+++ src/day-1/first/first.test.ts
@@ -4,7 +4,7 @@ import { strict as assert } from 'node:assert';
 import first from './first';
 
 describe('Day 1 first exercise tests', () => {
-  it('tests that fisrt and last digit in row are combined and sum with other rows', () => {
+  it('should combine the first and last digit in row and sum with other rows', () => {
     const input = `1abc2
     pqr3stu8vwx
     a1b2c3d4e5f
--- src/day-2/first/first.test.ts
+++ src/day-2/first/first.test.ts
@@ -4,7 +4,7 @@ import { strict as assert } from 'node:assert';
 import first from './first';
 
 describe('Day 2 first exercise tests', () => {
-  it('tests that game is possible verifying the balls don\'t exceed maximum', () => {
+  it('should get the game number of the possible games verifying the balls don\'t exceed maximum', () => {
     const input = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
     Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
     Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
